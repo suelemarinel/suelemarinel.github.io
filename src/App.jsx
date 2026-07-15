0eec4abe7050ef7e2f2ec3db8403cc33912d7051
Sue <sue.lemarinel@gmail.com>
2026-07-15 17:02:30 +0200

    feat: smooth scrolling with Lenis

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom"
+import SmoothScroll from "./components/SmoothScroll.jsx"
 import Home from "./pages/Home.jsx"
 import Portfolio from "./pages/Portfolio.jsx"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/portfolio" element={<Portfolio />} />
-    </Routes>
+    <>
+      <SmoothScroll />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ src/components/SmoothScroll.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react"
+import Lenis from "lenis"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
+gsap.registerPlugin(ScrollTrigger)
+
+function SmoothScroll() {
+  useEffect(() => {
+    const reduced = window.matchMedia(
+      "(prefers-reduced-motion: reduce)"
+    ).matches
+    if (reduced) return
+
+    const lenis = new Lenis({
+      duration: 1.1,
+      smoothWheel: true,
+    })
+
+    lenis.on("scroll", ScrollTrigger.update)
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000)
+    })
+    gsap.ticker.lagSmoothing(0)
+
+    return () => {
+      lenis.destroy()
+      gsap.ticker.remove(lenis.raf)
+    }
+  }, [])
+
+  return null
+}
+
+export default SmoothScroll
